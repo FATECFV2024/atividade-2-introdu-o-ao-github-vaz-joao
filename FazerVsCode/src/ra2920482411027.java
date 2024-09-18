937ad1e10b71e7431788ff5f133e844bdffc3892
João Victor Vaz <joao.vaz@fatec.sp.gov.br>
2024-09-18 10:59:36 -0300

    [feat] Atividade java

---
+++ FazerVsCode/src/ra2920482411027.java
@@ -0,0 +1,17 @@
+public class ra2920482411027{
+    public static void main(String[] args){
+        String nome = "João Victor da Silva Vaz";
+        String RGM = "2920482411027";
+        int idade = 20;
+        String github = "@vaz-joao";
+        String linkedin = "@joão-victor-vaz" ;
+        String turma = "3º ADS";
+
+        System.out.println("Nome Completo" + nome);
+        System.out.println("RA: " + RGM);
+        System.out.println("Idade: "+ idade);
+        System.out.println("Github: " + github);
+        System.out.println("LinkedIn: " + linkedin);
+        System.out.println("Turma: " + turma);
+    }
+}
